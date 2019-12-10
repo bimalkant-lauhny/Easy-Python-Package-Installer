1c0aa43d3ad99b1e36853411583c9fad8c162e2b
Bimalkant Lauhny <bklauhny@indodana.com>
2019-12-11 00:50:02 +0530

    Package structure

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    install_requires= fh.read().strip().split()
+
 setuptools.setup(
     name="easy-ppi",
     version="0.0.1",
@@ -11,11 +14,12 @@ setuptools.setup(
     description="Easy Interpreter for managing pip packages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/code-master5/Advanced-Python-Package-Installer",
+    url="https://github.com/code-master5/Easy-Python-Package-Installer",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Ubuntu 18.04",
+        "Operating System :: OS Independent",
     ],
 )
